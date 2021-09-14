645f9537554e5a056d7c288a97688c86926c8614
Raul Guerrero <raul.guerrero@linkcode.com.mx>
2021-09-13 22:24:53 -0500

    First commit!

---
+++ src/main/java/com/vallarta/VallartaPlusTrackingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.vallarta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VallartaPlusTrackingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VallartaPlusTrackingServiceApplication.class, args);
+
+    }
+}
